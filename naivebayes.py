08cf5a47cda32a798d13f7791f849bec96c03ccc
Yang Jin <yangjin2017@hotmail.com>
2020-05-13 17:35:38 -0500

    Add files via upload

---
+++ naivebayes.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+@author: Nigel
+"""
+
+import numpy as np
+from naivebayesPY import naivebayesPY
+from naivebayesPXY import naivebayesPXY
+
+def naivebayes(x, y, x1):
+# =============================================================================
+#function logratio = naivebayes(x,y,x1);
+#
+#Computation of log P(Y|X=x1) using Bayes Rule
+#Input:
+#x : n input vectors of d dimensions (dxn)
+#y : n labels (-1 or +1)
+#x1: input vector of d dimensions (dx1)
+#
+#Output:
+#logratio: log (P(Y = 1|X=x1)/P(Y=-1|X=x1))
+# =============================================================================
+
+
+    
+    # Convertng input matrix x and x1 into NumPy matrix
+    # input x and y should be in the form: 'a b c d...; e f g h...; i j k l...'
+    X = np.matrix(x)
+    X1= np.matrix(x1)
+    
+    # Pre-configuring the size of matrix X
+    d,n = X.shape
+    
+# =============================================================================
+# fill in code here
+    PositiveY, NegativeY = naivebayesPY(x,y)
+    PositiveXY, NegativeXY = naivebayesPXY(x,y)
+    
+    logratio = np.log(PositiveY) - np.log(NegativeY) + np.matmul(x1.T, np.log(PositiveXY)) - np.matmul(x1.T, np.log(NegativeXY))
+    
+    return logratio
+# =============================================================================
